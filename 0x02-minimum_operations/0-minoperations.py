96f7d90eb4a1097c781666acf9214762be28caa0
iiihassanii <hassanabdelazim.max@gmail.com>
2024-06-11 13:12:49 +0300

    task 2

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -22,14 +22,7 @@ Number of operations: 6
 
 
 def minOperations(n: int) -> int:
-    """_summary_
-
-    Args:
-        n (int): _description_
-
-    Returns:
-        int: _description_
-    """
+    """_summary_"""
     if n <= 1 or type(n) is not int:
         return 0
 
